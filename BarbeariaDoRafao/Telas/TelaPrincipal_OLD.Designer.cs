dcbce84cdf3111aad650efe99468401bd0072534
Kayque <kayquegoulart0@gmail.com>
2023-10-24 21:52:27 -0300

    Criação de TelaPrincipal OLD, foi criado uma nova, ainda falta alterações para fazer!

--- BarbeariaDoRafao/Telas/TelaPrincipal_OLD.Designer.cs
+++ BarbeariaDoRafao/Telas/TelaPrincipal_OLD.Designer.cs
@@ -28,17 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editarPerfilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmCadastrar = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmCadastrarCliente = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmCadastrarFuncionario = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmCadastarServico = new System.Windows.Forms.ToolStripMenuItem();
-            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmGerenciarAgenda = new System.Windows.Forms.ToolStripMenuItem();
-            this.TsmRelatorio = new System.Windows.Forms.ToolStripMenuItem();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal_OLD));
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.TslUserNome = new System.Windows.Forms.ToolStripStatusLabel();
@@ -46,95 +36,20 @@
             this.TslEmail = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
             this.TslPerfil = new System.Windows.Forms.ToolStripStatusLabel();
-            this.menuStrip1.SuspendLayout();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BtnCadastrar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnGerenciar = new System.Windows.Forms.Button();
+            this.BtnFuncionarios = new System.Windows.Forms.Button();
+            this.BtnFaturamento = new System.Windows.Forms.Button();
+            this.BtnAgenda = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.cadastroToolStripMenuItem,
-            this.TsmCadastrar,
-            this.aToolStripMenuItem,
-            this.TsmRelatorio});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(774, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // cadastroToolStripMenuItem
-            // 
-            this.cadastroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.editarPerfilToolStripMenuItem,
-            this.sairToolStripMenuItem});
-            this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
-            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
-            this.cadastroToolStripMenuItem.Text = "Meu Perfil";
-            // 
-            // editarPerfilToolStripMenuItem
-            // 
-            this.editarPerfilToolStripMenuItem.Name = "editarPerfilToolStripMenuItem";
-            this.editarPerfilToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
-            this.editarPerfilToolStripMenuItem.Text = "Editar Perfil";
-            // 
-            // sairToolStripMenuItem
-            // 
-            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
-            this.sairToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
-            this.sairToolStripMenuItem.Text = "Sair";
-            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
-            // 
-            // TsmCadastrar
-            // 
-            this.TsmCadastrar.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.TsmCadastrarCliente,
-            this.TsmCadastrarFuncionario,
-            this.TsmCadastarServico});
-            this.TsmCadastrar.Name = "TsmCadastrar";
-            this.TsmCadastrar.Size = new System.Drawing.Size(66, 20);
-            this.TsmCadastrar.Text = "Cadastro";
-            // 
-            // TsmCadastrarCliente
-            // 
-            this.TsmCadastrarCliente.Name = "TsmCadastrarCliente";
-            this.TsmCadastrarCliente.Size = new System.Drawing.Size(190, 22);
-            this.TsmCadastrarCliente.Text = "Cadastrar Cliente";
-            this.TsmCadastrarCliente.Click += new System.EventHandler(this.TsmCadastrarCliente_Click);
-            // 
-            // TsmCadastrarFuncionario
-            // 
-            this.TsmCadastrarFuncionario.Name = "TsmCadastrarFuncionario";
-            this.TsmCadastrarFuncionario.Size = new System.Drawing.Size(190, 22);
-            this.TsmCadastrarFuncionario.Text = "Cadastrar Funcionário";
-            this.TsmCadastrarFuncionario.Click += new System.EventHandler(this.TsmCadastrarFuncionario_Click);
-            // 
-            // TsmCadastarServico
-            // 
-            this.TsmCadastarServico.Name = "TsmCadastarServico";
-            this.TsmCadastarServico.Size = new System.Drawing.Size(190, 22);
-            this.TsmCadastarServico.Text = "Cadastrar Serviço";
-            this.TsmCadastarServico.Click += new System.EventHandler(this.TsmCadastarServico_Click);
-            // 
-            // aToolStripMenuItem
-            // 
-            this.aToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.TsmGerenciarAgenda});
-            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
-            this.aToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.aToolStripMenuItem.Text = "Agenda";
-            // 
-            // TsmGerenciarAgenda
-            // 
-            this.TsmGerenciarAgenda.Name = "TsmGerenciarAgenda";
-            this.TsmGerenciarAgenda.Size = new System.Drawing.Size(180, 22);
-            this.TsmGerenciarAgenda.Text = "Genrenciar Agenda";
-            // 
-            // TsmRelatorio
-            // 
-            this.TsmRelatorio.Name = "TsmRelatorio";
-            this.TsmRelatorio.Size = new System.Drawing.Size(66, 20);
-            this.TsmRelatorio.Text = "Relatório";
             // 
             // statusStrip1
             // 
@@ -145,9 +60,9 @@
             this.TslEmail,
             this.toolStripStatusLabel4,
             this.TslPerfil});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 560);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1019);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(774, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1904, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -190,45 +105,166 @@
             this.TslPerfil.Size = new System.Drawing.Size(98, 17);
             this.TslPerfil.Text = "PerfilNivelAcesso";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(292, 992);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label1.Location = new System.Drawing.Point(113, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 79);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Raff\'s Barber";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label2.Location = new System.Drawing.Point(343, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "™";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(177, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 90);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BtnCadastrar
+            // 
+            this.BtnCadastrar.BackColor = System.Drawing.Color.White;
+            this.BtnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCadastrar.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCadastrar.Location = new System.Drawing.Point(147, 228);
+            this.BtnCadastrar.Name = "BtnCadastrar";
+            this.BtnCadastrar.Size = new System.Drawing.Size(193, 51);
+            this.BtnCadastrar.TabIndex = 6;
+            this.BtnCadastrar.Text = "Cadastrar ";
+            this.BtnCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label3.Location = new System.Drawing.Point(113, 864);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(258, 79);
+            this.label3.TabIndex = 11;
+            this.label3.Text = " Since 2002 ®";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnGerenciar
+            // 
+            this.BtnGerenciar.BackColor = System.Drawing.Color.White;
+            this.BtnGerenciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGerenciar.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGerenciar.Location = new System.Drawing.Point(147, 342);
+            this.BtnGerenciar.Name = "BtnGerenciar";
+            this.BtnGerenciar.Size = new System.Drawing.Size(193, 51);
+            this.BtnGerenciar.TabIndex = 12;
+            this.BtnGerenciar.Text = "Gerenciar";
+            this.BtnGerenciar.UseVisualStyleBackColor = false;
+            // 
+            // BtnFuncionarios
+            // 
+            this.BtnFuncionarios.BackColor = System.Drawing.Color.White;
+            this.BtnFuncionarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFuncionarios.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFuncionarios.Location = new System.Drawing.Point(147, 609);
+            this.BtnFuncionarios.Name = "BtnFuncionarios";
+            this.BtnFuncionarios.Size = new System.Drawing.Size(193, 51);
+            this.BtnFuncionarios.TabIndex = 13;
+            this.BtnFuncionarios.Text = "Funcionários";
+            this.BtnFuncionarios.UseVisualStyleBackColor = false;
+            // 
+            // BtnFaturamento
+            // 
+            this.BtnFaturamento.BackColor = System.Drawing.Color.White;
+            this.BtnFaturamento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFaturamento.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnFaturamento.Location = new System.Drawing.Point(147, 751);
+            this.BtnFaturamento.Name = "BtnFaturamento";
+            this.BtnFaturamento.Size = new System.Drawing.Size(193, 51);
+            this.BtnFaturamento.TabIndex = 14;
+            this.BtnFaturamento.Text = "Faturamento";
+            this.BtnFaturamento.UseVisualStyleBackColor = false;
+            // 
+            // BtnAgenda
+            // 
+            this.BtnAgenda.BackColor = System.Drawing.Color.White;
+            this.BtnAgenda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAgenda.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgenda.Location = new System.Drawing.Point(147, 466);
+            this.BtnAgenda.Name = "BtnAgenda";
+            this.BtnAgenda.Size = new System.Drawing.Size(193, 51);
+            this.BtnAgenda.TabIndex = 15;
+            this.BtnAgenda.Text = "Agenda";
+            this.BtnAgenda.UseVisualStyleBackColor = false;
+            // 
             // TelaPrincipal_OLD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(774, 582);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.BtnAgenda);
+            this.Controls.Add(this.BtnFaturamento);
+            this.Controls.Add(this.BtnFuncionarios);
+            this.Controls.Add(this.BtnGerenciar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnCadastrar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "TelaPrincipal_OLD";
             this.Text = "TelaPrincipal";
             this.Load += new System.EventHandler(this.TelaPrincipal_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripMenuItem TsmCadastrar;
-        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem TsmRelatorio;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel TslUserNome;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
         private System.Windows.Forms.ToolStripStatusLabel TslEmail;
-        private System.Windows.Forms.ToolStripMenuItem editarPerfilToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem TsmCadastrarCliente;
-        private System.Windows.Forms.ToolStripMenuItem TsmCadastrarFuncionario;
-        private System.Windows.Forms.ToolStripMenuItem TsmCadastarServico;
-        private System.Windows.Forms.ToolStripMenuItem TsmGerenciarAgenda;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
         private System.Windows.Forms.ToolStripStatusLabel TslPerfil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button BtnCadastrar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnGerenciar;
+        private System.Windows.Forms.Button BtnFuncionarios;
+        private System.Windows.Forms.Button BtnFaturamento;
+        private System.Windows.Forms.Button BtnAgenda;
     }
 }
